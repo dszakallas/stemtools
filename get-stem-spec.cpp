9b091cb7f5f325172e0ecd93e837491d2b05a8d5
Dávid Szakállas <david.szakallas@gmail.com>
2024-03-29 16:21:05 +0100

    fix get-stem-spec

--- get-stem-spec.cpp
+++ get-stem-spec.cpp
@@ -5,7 +5,7 @@ constexpr uint8_t atomPreambleSize = 8;
 constexpr uint8_t extendedSizeFieldSize = 8;
 
 constexpr uint32_t be32(const char* bytes) {
-  return (bytes[0] << 24) | (bytes[1] << 16) | (bytes[2] << 8) | bytes[3];
+  return ((uint8_t)bytes[0] << 24) | ((uint8_t)bytes[1] << 16) | ((uint8_t)bytes[2] << 8) | (uint8_t)bytes[3];
 }
 
 constexpr uint64_t be64(const char* bytes) {
@@ -16,85 +16,75 @@ constexpr auto moovAtom = be32("moov");
 constexpr auto udtaAtom = be32("udta");
 constexpr auto stemAtom = be32("stem");
 
-std::string _atomTypeToString(uint32_t atomType) {
-  std::string result;
-  result.push_back((atomType >> 24) & 0xff);
-  result.push_back((atomType >> 16) & 0xff);
-  result.push_back((atomType >> 8) & 0xff);
-  result.push_back(atomType & 0xff);
-  return result;
+bool read(std::ifstream& stream, char* buffer, uint64_t size) {
+  stream.read(buffer, size);
+  return stream.gcount() == size;
 }
 
 int main(int argc, char** argv) {
   if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
+    std::cerr << "usage: " << argv[0] << " <filename>" << std::endl;
     return 1;
   }
 
   std::ifstream file(argv[1], std::ios::binary);
   if (!file) {
-    std::cerr << "Cannot open file " << argv[1] << std::endl;
+    std::cerr << "cannot open file " << argv[1] << std::endl;
     return -1;
   }
 
+  char atomBytes[atomPreambleSize], extendedSizeBytes[extendedSizeFieldSize];
+  uint64_t bytesRead, atomSize;
+  uint32_t atomType;
   std::vector<uint64_t> openAtoms;
 
   while (true) {
-    if (file.eof()) {
-      break;
-    }
-    if (!openAtoms.empty() && !openAtoms.back()) {
-      openAtoms.pop_back();
-      continue;
+    if (file.eof() || (openAtoms.size() && !openAtoms.back())) {
+      // read file or consumed a child atom without advancing to next level
+      std::cerr << "not a stem file" << std::endl;
+      return 1;
     }
 
     // read atom preamble
-    char atomBytes[atomPreambleSize];
-    file.read(atomBytes, atomPreambleSize);
-    if (file.gcount() != atomPreambleSize) {
-      break;
+    if (!read(file, atomBytes, atomPreambleSize)) {
+      std::cerr << "failed to read atom preamble" << std::endl;
+      return 1;
     }
-    uint64_t bytesRead = 0;
-    bytesRead += atomPreambleSize;
 
-    uint64_t atomSize = be32(atomBytes);
+    bytesRead = atomPreambleSize;
+    atomSize = be32(atomBytes);
 
     if (atomSize == 1) {
-      std::cerr << "Extended size field" << std::endl;
-      char extendedSizeBytes[extendedSizeFieldSize];
-      file.read(extendedSizeBytes, extendedSizeFieldSize);
-      if (file.gcount() != extendedSizeFieldSize) {
-        break;
+      if (!read(file, extendedSizeBytes, extendedSizeFieldSize)) {
+        std::cerr << "failed to read extended atom" << std::endl;
+        return 1;
       }
       bytesRead += extendedSizeFieldSize;
       atomSize = be64(extendedSizeBytes);
     }
-    uint32_t atomType = be32(atomBytes + 4);
-
-    // process atom
-    for (int i = 0; i < openAtoms.size(); i++) {
-      std::cerr << "\t";
-    }
-    std::cerr << _atomTypeToString(atomType) << "[" << atomSize << "]" << std::endl;
+    atomType = be32(atomBytes + 4);
 
     // commit to consuming the rest of the atom
     if (!openAtoms.empty()) {
       openAtoms.back() -= atomSize;
     }
 
+    atomSize -= bytesRead;
+
     if (
-        !openAtoms.size() && atomType == moovAtom ||
+        openAtoms.size() == 0 && atomType == moovAtom ||
         openAtoms.size() == 1 && atomType == udtaAtom){
-      openAtoms.push_back(atomSize - bytesRead);
+      openAtoms.push_back(atomSize);
     } else if (openAtoms.size() == 2 && atomType == stemAtom) {
-      char stemData[atomSize - bytesRead];
-      file.read(stemData, atomSize - bytesRead);
-      if (file.gcount() != atomSize - bytesRead) {
-        break;
+      std::string stemData(atomSize, 0);
+      if (!read(file, stemData.data(), atomSize)) {
+        std::cerr << "failed to read stem metadata" << std::endl;
+        return 1;
       }
-      std::cout << std::string(stemData, atomSize - bytesRead) << std::endl;
+      std::cout << stemData << std::endl;
+      break;
     } else {
-      file.seekg(atomSize - bytesRead, std::ios::cur);
+      file.seekg(atomSize, std::ios::cur);
     }
   }
   return 0;
